7662111abba4bf41fe01d93ed2af4f2de7c8dc2f
Condorbox <minecraft13122001@gmail.com>
2022-05-20 21:04:40 +0200

    readTable and readTableWithLabels in csv no longer share code

--- Practica1/src/main/java/CSV/CSV.java
+++ Practica1/src/main/java/CSV/CSV.java
@@ -4,24 +4,38 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class CSV{
+public class CSV {
     private static final Table<Row> TABLE_ERROR = new Table<Row>();
     private static final TableWithLabel TABLE_WITH_LABEL_ERROR = new TableWithLabel();
-
     private final String delimiter;
 
-    public CSV(){
+    public CSV() {
         this.delimiter = ",";
     }
 
-    public CSV(String delimiter){
+    public CSV(String delimiter) {
         this.delimiter = delimiter;
     }
 
     public Table<Row> readTable(String csvFile) {
+        Table<Row> table = new Table<Row>();
+        table = readCSV(csvFile, table, false);
+        if (table == null)
+            return TABLE_ERROR;
+        return table;
+    }
+
+    public TableWithLabel readTableWithLabels(String csvFileWithLabels) {
+        TableWithLabel table = new TableWithLabel();
+        table = readCSV(csvFileWithLabels, table, true);
+        if (table == null)
+            return TABLE_WITH_LABEL_ERROR;
+        return table;
+    }
+
+    private <T extends Table> T readCSV(String csvFile, T table, boolean withLabels) {
         try {
             Scanner myReader = new Scanner(new File(csvFile));
-            Table<Row> table = new Table<Row>();
             boolean headersRead = false;
             while (myReader.hasNextLine()) {
                 String[] line = myReader.nextLine().split(delimiter);
@@ -31,16 +45,13 @@ public class CSV{
                     }
                     headersRead = true;
                 } else {
-                    Row newRow = new Row();
-                    for (String data : line) {
-                        try {
-                            Double dataToAdd = Double.parseDouble(data);
-                            newRow.addData(dataToAdd);
-                        } catch (Exception exception) {
-                            System.out.println("Wrong csv format, data can be only numbers");
-                            exception.printStackTrace();
-                            return TABLE_ERROR;
-                        }
+                    RowWithLabel newRow;
+                    try {
+                        newRow = lineToRow(line, table.getHeaders().size() - 1, withLabels);
+                    }catch (NumberFormatException exception){
+                        System.out.println("Wrong csv format");
+                        exception.printStackTrace();
+                        return null;
                     }
                     table.addRow(newRow);
                 }
@@ -49,46 +60,20 @@ public class CSV{
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
-            return TABLE_ERROR;
+            return null;
         }
     }
 
-    public TableWithLabel readTableWithLabels(String csvFileWithLabels) {
-        try{
-            Scanner myReader = new Scanner(new File(csvFileWithLabels));
-            TableWithLabel table = new TableWithLabel();
-            boolean headersRead = false;
-            while (myReader.hasNextLine()){
-                String[] line = myReader.nextLine().split(delimiter);
-                if(!headersRead){ //Header
-                    for (String header : line){
-                        table.addHeader(header);
-                    }
-                    headersRead = true;
-                }else{
-                    RowWithLabel newRow = new RowWithLabel();
-                    for (int i = 0; i<line.length; i++){
-                        if (i == table.getHeaders().size() - 1){ //Label
-                            newRow.setLabel(line[i]);
-                        }else{
-                            try {
-                                Double dataToAdd = Double.parseDouble(line[i]);
-                                newRow.addData(dataToAdd);
-                            }catch (Exception exception){
-                                System.out.println("Wrong csv format, data can be only numbers except for the label");
-                                exception.printStackTrace();
-                                return TABLE_WITH_LABEL_ERROR;
-                            }
-                        }
-                    }
-                    table.addRow(newRow);
-                }
+    private RowWithLabel lineToRow(String[] line, int labelLine, boolean withLabels) throws NumberFormatException {
+        RowWithLabel newRow = new RowWithLabel();
+        for (int i = 0; i < line.length; i++) {
+            if (i == labelLine && withLabels) { //Label
+                newRow.setLabel(line[i]);
+            } else {
+                Double dataToAdd = Double.parseDouble(line[i]);
+                newRow.addData(dataToAdd);
             }
-            return table;
-        }catch (FileNotFoundException e){
-            System.out.println("File not found");
-            e.printStackTrace();
-            return TABLE_WITH_LABEL_ERROR;
         }
+        return newRow;
     }
 }
